64abcc9fa2aebcc1aa97d7dfc1bb4eb6e6edbdd7
MeowthPxnk <meowthpxnk@gmail.com>
2024-09-18 02:50:48 +0300

    123

--- app/devices.py
+++ app/devices.py
@@ -5,10 +5,10 @@ from app.utils.devices_settings import get_devices_settings
 def analyse_devices():
     processes = get_processes()
     # settings = get_devices_settings()
-    # print(processes)
+    print(processes)
     # print(len(processes))
 
-    for p in processes:
-        if not p.port:
-            print(p)
+    # for p in processes:
+    #     if not p.port:
+    #         print(p)
     # print(settings)

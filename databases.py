e5b6185c6535f291d1584db3378e5218618b14a5
MeowthPxnk <meowthpxnk@gmail.com>
2024-09-17 22:34:52 +0300

    123

--- databases.py
+++ databases.py
@@ -1,32 +1,27 @@
 from pymongo import MongoClient
 
-# Подключаемся к серверу MongoDB
-client = MongoClient("mongodb://localhost:27017/")  # Замените URL на ваш
 
-# Получаем информацию о текущих операциях
-current_ops = client.admin.command("currentOp")
+def get_database_connections():
+    # Подключаемся к серверу MongoDB
+    client = MongoClient("mongodb://localhost:27017/")  # Замените URL на ваш
 
-# Извлекаем текущие операции
-operations = current_ops.get("inprog", [])
+    # Получаем информацию о текущих операциях
+    current_ops = client.admin.command("currentOp")
 
-# Выводим информацию о текущих операциях и клиентах
-for op in operations:
-    opid = op.get("opid", "N/A")
-    op_type = op.get("op", "N/A")
-    ns = op.get("ns", "N/A")
-    query = op.get("query", "N/A")
-    client_info = op.get("client", "N/A")
+    # Извлекаем текущие операции
+    operations = current_ops.get("inprog", [])
 
-    # Извлекаем clientMetadata
-    client_metadata = op.get("clientMetadata", {})
-    driver_name = client_metadata.get("driver", {}).get("name", "N/A")
-    driver_version = client_metadata.get("driver", {}).get("version", "N/A")
+    connections = []
 
-    print(f"Operation ID: {opid}")
-    print(f"Operation Type: {op_type}")
-    print(f"Namespace: {ns}")
-    print(f"Query: {query}")
-    print(f"Client: {client_info}")
-    print(f"Driver Name: {driver_name}")
-    print(f"Driver Version: {driver_version}")
-    print("-" * 40)
+    # Выводим информацию о текущих операциях и клиентах
+    for op in operations:
+        client_metadata = op.get("clientMetadata", {})
+        driver_name = client_metadata.get("driver", {}).get("name", "N/A")
+        if driver_name == "nodejs":
+            connections.append(op)
+    return connections
+
+
+if __name__ == "__main__":
+    connections = get_database_connections()
+    print(f"Connections count: {len(connections)}")

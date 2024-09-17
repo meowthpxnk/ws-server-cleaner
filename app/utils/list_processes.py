c6884c120c4a0e33560fb6aef78fdc1e9ae88e65
MeowthPxnk <meowthpxnk@gmail.com>
2024-09-17 23:47:00 +0300

    123

--- app/utils/list_processes.py
+++ app/utils/list_processes.py
@@ -13,7 +13,7 @@ class Process:
         return f"<ClProcess: pid={self.pid}, port={self.port}>"
 
 
-def get_processes() -> list[Process]:
+def get_processes():
     processes = []
     for proc in psutil.process_iter(["pid", "name", "cmdline"]):
 

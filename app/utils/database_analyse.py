5031d34d9a88aef5f2f9e3364ee2cccc8aec69ae
MeowthPxnk <meowthpxnk@gmail.com>
2024-09-18 02:53:18 +0300

    123

--- app/utils/database_analyse.py
+++ app/utils/database_analyse.py
@@ -1,7 +1,14 @@
 from pymongo import MongoClient
 
+DB_NAME = "WA_MD_{phone}_1"
 
-def analyse_db(db_name):
+
+def analyse_db(phone):
     cl = MongoClient()
-    db = cl[db]
-    print(db)
+    db_name = DB_NAME.format(phone=phone)
+    # db = cl[db]
+    print(db_name)
+
+
+if __name__ == "__main__":
+    analyse_db("7999")

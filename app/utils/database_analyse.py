565dc0ee4be274956c88448c35a4c0460f582b97
MeowthPxnk <meowthpxnk@gmail.com>
2024-09-18 03:16:26 +0300

    123

--- app/utils/database_analyse.py
+++ app/utils/database_analyse.py
@@ -6,7 +6,7 @@ DB_NAME = "WA_MD_{phone}_1"
 def analyse_db(phone):
     cl = MongoClient()
     db_name = DB_NAME.format(phone=phone)
-    db = cl[db]
+    db = cl[db_name]
 
     print(db.__dir__())
 

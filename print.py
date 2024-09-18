6296e3bd6f129fbd2d670d43550b833cd37486ae
MeowthPxnk <meowthpxnk@gmail.com>
2024-09-18 16:39:47 +0300

    123

--- print.py
+++ print.py
@@ -2,8 +2,8 @@ from app.devices import analyse_devices
 from app.db import analyse_dbs, clear_all_dbs, drop_inactive_dbs, get_db_phones
 from app.files import clear_folder
 
-# analyse_devices()
+analyse_devices()
 
 # analyse_dbs()
-phones = get_db_phones()
-clear_folder(phones)
+# phones = get_db_phones()
+# clear_folder(phones)

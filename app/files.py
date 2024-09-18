0ae8db6f399fe80e88bfe50117615717e6733adc
MeowthPxnk <meowthpxnk@gmail.com>
2024-09-18 16:31:18 +0300

    123

--- app/files.py
+++ app/files.py
@@ -3,5 +3,5 @@ from app.utils.devices_settings import get_devices_folders, ROOT_PATH
 
 def clear_folder(acitve_phones):
     print(len(acitve_phones))
-    print(len(devices))
     devices = get_devices_folders()
+    print(len(devices))

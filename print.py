f3f455df761160907a95f026257f82f72abce8f3
MeowthPxnk <meowthpxnk@gmail.com>
2024-09-18 16:22:25 +0300

    123

--- print.py
+++ print.py
@@ -1,7 +1,7 @@
 from app.devices import analyse_devices
-from app.db import analyse_dbs, clear_all_dbs, drop_inactive_dbs
+from app.db import analyse_dbs, clear_all_dbs, drop_inactive_dbs, get_db_phones
 
 # analyse_devices()
 
 # analyse_dbs()
-drop_inactive_dbs()
+get_db_phones()

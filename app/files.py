1a266dff425273dca0635c82810a9b79d00a150d
MeowthPxnk <meowthpxnk@gmail.com>
2024-09-18 16:37:33 +0300

    123

--- app/files.py
+++ app/files.py
@@ -1,14 +1,16 @@
 from app.utils.devices_settings import get_devices_folders, ROOT_PATH
+import os
+import shutil
 
 
 def clear_folder(acitve_phones):
     folders = get_devices_folders()
-    print(len(acitve_phones))
-    print(len(folders))
 
     to_remove = []
     for folder in folders:
         if folder not in acitve_phones:
             to_remove.append(folder)
 
-    print(len(to_remove))
+    for rem_folder in to_remove:
+        print(f"Removing folder for device {rem_folder}")
+        shutil.rmtree(os.path.join(ROOT_PATH, rem_folder))

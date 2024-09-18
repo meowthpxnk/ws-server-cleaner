84b99d936250c3ac614d44fee370dc2bb0b88f23
MeowthPxnk <meowthpxnk@gmail.com>
2024-09-18 03:19:40 +0300

    123

--- app/utils/database_analyse.py
+++ app/utils/database_analyse.py
@@ -3,12 +3,36 @@ from pymongo import MongoClient
 DB_NAME = "WA_MD_{phone}_1"
 
 
+DB_SEND = "sentMessages"
+DB_CONT = "contacts"
+DB_FAILED = "failedMessages"
+DB_READ = "readMessages"
+DB_IN = "inMessages"
+DB_META = "meta"
+DB_FROM_WA = "fromWaQueue"
+
+DB_COLLECTIONS = [
+    DB_SEND,
+    DB_CONT,
+    DB_FAILED,
+    DB_READ,
+    DB_IN,
+    DB_META,
+    DB_FROM_WA,
+]
+
+
 def analyse_db(phone):
     cl = MongoClient()
     db_name = DB_NAME.format(phone=phone)
     db = cl[db_name]
 
-    print(db.list_collection_names())
+    for coll in DB_COLLECTIONS:
+        coll = db[coll]
+
+        print(coll.__dir__())
+
+    # print(db.list_collection_names())
 
 
 if __name__ == "__main__":

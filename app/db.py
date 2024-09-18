d36da039e7d8d3d2b74c493b1628a4d914d14f7e
MeowthPxnk <meowthpxnk@gmail.com>
2024-09-18 15:08:44 +0300

    123

---
+++ app/db.py
@@ -0,0 +1,8 @@
+from pymongo import MongoClient
+
+
+def analyse_db():
+    cl = MongoClient()
+
+    print(cl.list_databases())
+    ...
--- print.py
+++ print.py
@@ -1,3 +1,6 @@
 from app.devices import analyse_devices
+from app.db import analyse_db
 
-analyse_devices()
+# analyse_devices()
+
+analyse_db()

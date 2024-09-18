debe85c23659bfb8ee4c62375c029b97404dfc38
MeowthPxnk <meowthpxnk@gmail.com>
2024-09-18 15:12:33 +0300

    123

--- app/db.py
+++ app/db.py
@@ -1,12 +1,16 @@
 from pymongo import MongoClient
 
-from app.utils.database_analyse import analyse_db
+from app.utils.database_analyse import analyse_db, clear_db
 
 
 def analyse_dbs():
     # cl = MongoClient()
 
     # print(cl.list_database_names())
+
+    phone = "12059906323"
+
+    clear_db(phone)
 
     analyse_db("12059906323")
     ...
